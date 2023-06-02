c1de3b17641da30637d71a2e2ba1916b830a58ec
Thomas Aldaitz <thomas.aldaitz@gmail.com>
2023-06-02 08:58:44 +0200

    Add the Kata Add Time

---
+++ kata.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Ajouter 5 secondes à chaque heure du tableau suivant.
+ * Les minutes et les heures doivent naturellement évoluer.
+ * 
+ */
+
+ $times = [
+    ['hours' => 12, 'minutes'=> 34, 'seconds' => 45],
+    ['hours' => 8, 'minutes'=> 26, 'seconds' => 59],
+    ['hours' => 17, 'minutes'=> 38, 'seconds' => 58],
+    ['hours' => 21, 'minutes'=> 59, 'seconds' => 28],
+    ['hours' => 23, 'minutes'=> 59, 'seconds' => 59],
+ ];
